d35131e306582dbbea120d1a20463dfcab54c74e
pannavich <pannavich1911@gmail.com>
2024-03-24 02:04:33 +0700

    feat: make frame cooler

--- app/frame/trending/frames/route.tsx
+++ app/frame/trending/frames/route.tsx
@@ -49,8 +49,21 @@ const handleRequest = frames((async (ctx: any) => {
     return {
       image: (
         <span>
-          <div style={{ display: "flex", flexDirection: "column" }}>
-            Discover Trending Frames
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "center", // Centers content vertically in the container
+              alignItems: "center", // Centers content horizontally in the container
+              height: "100vh", // Takes up the full viewport height
+              width: "100%",
+              background: "linear-gradient(135deg, #f3fd28, #f97c00)", // Cool gradient background
+              color: "white", // Text color
+              fontSize: "48px", // Text size
+              textShadow: "2px 2px 4px rgba(0, 0, 0, 0.5)", // Text shadow for better readability
+            }}
+          >
+            🔥 Discover Trending Frames ⚡️
           </div>
         </span>
       ),
@@ -63,6 +76,9 @@ const handleRequest = frames((async (ctx: any) => {
         </Button>,
         <Button action="post" target={{ query: { pageIndex: 1, hour: 24 } }}>
           24 h
+        </Button>,
+        <Button action="link" target={currentURL("/").toString()}>
+          View site
         </Button>,
       ],
       accepts: acceptedProtocols,
