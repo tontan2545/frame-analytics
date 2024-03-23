ab58135f5b9f1048a9af95d923300e1fbca092ce
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-03-23 16:03:37 +0700

    feat: batch update

--- app/api/etl/update/route.ts
+++ app/api/etl/update/route.ts
@@ -19,38 +19,48 @@ export async function GET(request: NextRequest) {
     );
   }
 
+  const now = new Date().toISOString();
   const allFrames: { hash: string }[] = await sql`
-    SELECT hash FROM frames
+    SELECT hash FROM frames ORDER BY timestamp DESC LIMIT 50000
   `;
 
-  const now = new Date().toISOString();
-  const updatedStats: {
+  interface UpdatedStats {
     hash: string;
     timestamp: string;
     replies: number;
     recasts: number;
     likes: number;
-  }[] = [];
-
-  for (const frames of _.chunk(allFrames, 500)) {
-    const casts = await neynarClient.fetchBulkCasts(frames.map((e) => e.hash));
-
-    casts.result.casts.forEach((cast) => {
-      updatedStats.push({
-        hash: cast.hash,
-        timestamp: now,
-        replies: cast.replies.count || 0,
-        recasts: cast.reactions.recasts.length || 0,
-        likes: cast.reactions.likes.length || 0,
-      });
-    });
   }
 
-  await sql`
-    INSERT INTO stats ${sql(updatedStats)} ON CONFLICT DO NOTHING
-  `.execute();
+  let i = 0;
+  const updated = await Promise.all(
+    _.chunk(allFrames, 350).map(async (frames) => {
+      const casts = await neynarClient.fetchBulkCasts(
+        frames.map((e) => e.hash)
+      );
+      const updatedStats = casts.result.casts.map(
+        (cast) =>
+          ({
+            hash: cast.hash,
+            timestamp: now,
+            replies: cast.replies.count || 0,
+            recasts: cast.reactions.recasts.length || 0,
+            likes: cast.reactions.likes.length || 0,
+          } as UpdatedStats)
+      );
+
+      await sql`
+      INSERT INTO stats ${sql(updatedStats)} ON CONFLICT DO NOTHING
+      `.execute();
+
+      return updatedStats.length;
+    })
+  );
+  updated.forEach((e) => (i += e));
 
   return Response.json({
-    message: `Inserted ${updatedStats.length} updates`,
+    message: `Inserted ${i}/${allFrames.length} updates in ${
+      new Date().getTime() - new Date(now).getTime()
+    }ms`,
   });
 }
