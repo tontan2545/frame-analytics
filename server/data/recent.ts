030d73a858c874f82028e824b47e9e2ace5368fd
Pakorn Nathong <pakorn.nth@hotmail.com>
2024-03-24 16:28:12 +0700

    feat: use new sql query

--- server/data/recent.ts
+++ server/data/recent.ts
@@ -1,8 +1,26 @@
 import sql from "../lib/postgres";
 
-export const getRecentFrames = async () => {
+export const getRecentFrames = async ({ limit = 10 }: { limit?: number }) => {
   const recentFrames: FrameRowWithStats[] = await sql`
-  SELECT frames.*, stats.likes, stats.recasts, stats.replies FROM frames LEFT JOIN stats ON stats.hash = frames.hash WHERE frames IS NOT NULL ORDER BY frames.timestamp DESC LIMIT 20
+      SELECT
+    	frames.*,
+    	s.likes,
+    	s.recasts,
+    	s.replies
+    FROM
+    	frames
+    	LEFT JOIN (
+    		SELECT
+    			ROW_NUMBER() OVER (PARTITION BY HASH ORDER BY timestamp DESC) AS rn,
+    			*
+    		FROM
+    			stats) s ON s.hash = frames.hash
+    		AND rn = 1
+    WHERE
+    	frames IS NOT NULL
+    ORDER BY
+        frames.timestamp DESC
+    LIMIT ${limit}
   `;
   return recentFrames;
 };
--- server/data/trending.ts
+++ server/data/trending.ts
@@ -9,39 +9,28 @@ export const getTrendingFrames = async ({
   intervalHour = 24,
   trendingLimit = 5,
 }: Props) => {
-  const trendingFrames: FrameRowWithStats[] = await sql`
-  SELECT DISTINCT ON (sub.hash) sub.hash, sub.author, sub.embeds, sub.frames, sub.timestamp, sub.text, sub.likes, sub.replies, sub.recasts
-  FROM (
-      SELECT frames.hash, frames.author, frames.embeds, frames.frames, frames.timestamp, frames.text, stats.likes, stats.replies, stats.recasts
-      FROM frames
-      JOIN stats ON frames.hash = stats.hash
-      WHERE frames.timestamp > NOW() - INTERVAL '1 hour' * ${intervalHour}
-      AND frames.frames IS NOT NULL
-      ORDER BY stats.likes + stats.replies + stats.recasts DESC
-      LIMIT ${trendingLimit * 20}
-  ) AS sub
-  ORDER BY sub.hash, sub.likes + sub.replies + sub.recasts DESC
-  LIMIT ${trendingLimit}
+  const trendingFrames = await sql`
+    SELECT
+    	frames.*,
+    	s.likes,
+    	s.recasts,
+    	s.replies
+    FROM
+    	frames
+    	LEFT JOIN (
+    		SELECT
+    			ROW_NUMBER() OVER (PARTITION BY HASH ORDER BY timestamp DESC) AS rn,
+    			*
+    		FROM
+    			stats) s ON s.hash = frames.hash
+    		AND rn = 1
+    WHERE
+    	frames IS NOT NULL AND frames.timestamp > NOW() - INTERVAL '1 hour' * ${intervalHour}
+    ORDER BY
+    	COALESCE(s.likes + s.recasts + s.replies, 0)
+    	DESC
+    LIMIT ${trendingLimit}
   `;
 
-  const response = [];
-  for (const frame of trendingFrames) {
-    const author: Author = frame.author;
-    const embeds: Embed[] = frame.embeds;
-    const frames: Frame[] = frame.frames;
-    response.push({
-      hash: frame.hash,
-      author: author,
-      embeds: embeds,
-      frames: frames,
-      parent_hash: frame.parent_hash,
-      timestamp: frame.timestamp,
-      text: frame.text,
-      likes: frame.likes,
-      replies: frame.replies,
-      recasts: frame.recasts,
-    });
-  }
-
-  return response;
+  return trendingFrames;
 };
