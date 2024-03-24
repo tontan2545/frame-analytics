612f3910a94c2674a14c9b707500d880245579b6
tontan2545 <tontan2545@gmail.com>
2024-03-24 16:10:44 +0700

    feat: interval select

---
+++ components/trending-interval-select.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import { Button } from "./ui/button";
+import Link from "next/link";
+
+type Props = {
+  interval?: string;
+};
+
+const TrendingIntervalSelect = ({ interval }: Props) => {
+  console.log(interval);
+  return (
+    <div className="border-border p-1 rounded-full border-2 w-96 h-10 flex gap-1">
+      <Link href="/" className="h-full w-full">
+        <Button
+          variant={!interval || interval === "72" ? "default" : "ghost"}
+          className="h-full w-full px-4 rounded-full"
+        >
+          3 days
+        </Button>
+      </Link>
+      <Link href="/?interval=24" className="h-full w-full">
+        <Button
+          variant={interval === "24" ? "default" : "ghost"}
+          className="h-full w-full px-4 rounded-full"
+        >
+          24 hours
+        </Button>
+      </Link>
+      <Link href="/?interval=6" className="h-full w-full">
+        <Button
+          variant={interval === "6" ? "default" : "ghost"}
+          className="h-full w-full px-4 rounded-full"
+        >
+          6 hours
+        </Button>
+      </Link>
+      <Link href="/?interval=1" className="h-full w-full">
+        <Button
+          variant={interval === "1" ? "default" : "ghost"}
+          className="h-full w-full px-4 rounded-full"
+        >
+          1 hour
+        </Button>
+      </Link>
+    </div>
+  );
+};
+
+export default TrendingIntervalSelect;
