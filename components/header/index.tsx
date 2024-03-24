5355e6bd82e21e6e838a8370827f58a7c7b31d5c
tontan2545 <tontan2545@gmail.com>
2024-03-24 19:02:13 +0700

    style: header logo

--- components/header/index.tsx
+++ components/header/index.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Button } from "../ui/button";
 import Search from "./search";
+import Image from "next/image";
+import Link from "next/link";
 
 type Props = {};
 
 const Header = (props: Props) => {
   return (
     <div className="sticky top-0 z-50 flex justify-between items-center px-10 py-3 backdrop-blur-lg border-b-[1px] border-transparent duration-300 transition-all hover:border-secondary bg-background/20">
-      <p>Logo</p>
+      <Link href="/">
+        <Image src="/logo.png" alt="logo" width={40} height={40} />
+      </Link>
       <Search />
       <div />
     </div>
